365f4b5906879d83b9e8544b6140ef94cb572527
Tom Beerbower <thomas@techelevator.com>
2023-06-27 21:13:54 -0400

    add lecture-final for week1

---
+++ java/module-1/week-1/REVIEW/lecture-final/src/main/java/com/techelevator/Example1.java
@@ -0,0 +1,47 @@
+package com.techelevator;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+
+		/*
+		1. What is 5 divided by 2?
+		*/
+		System.out.println(5/2);
+
+		int answer = 5 / 2;
+		System.out.println(answer);
+
+		/*
+		2. What is 5.0 divided by 2? (Or 5 divided by 2.0?)
+		*/
+
+		System.out.println(5.0 / 2);
+
+
+		/*
+		3. What is 66.6 divided by 100?
+		*/
+		System.out.println(66.6 / 100);
+
+		/*
+		4. If I divide 5 by 2, what's my remainder?
+		*/
+		System.out.println(5 % 2);
+
+		/*
+		5. What is 1,000,000,000 * 3?
+		*/
+
+		long billion = 1000000000L;
+		long threeBillion = billion * 3;
+
+//		int billion = 1000000000;
+//		System.out.println(billion);
+//		int threeBillion = billion * 3;
+
+		System.out.println(threeBillion);
+
+		
+	}
+}
