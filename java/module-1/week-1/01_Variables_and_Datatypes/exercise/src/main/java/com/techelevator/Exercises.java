e2c3c03eae9bc3a1420b15c3b4c8015f31fafec5
Brent Winters <brentwinters100@gmail.com>
2023-06-28 18:04:02 -0400

    Fixed Test #3

--- java/module-1/week-1/01_Variables_and_Datatypes/exercise/src/main/java/com/techelevator/Exercises.java
+++ java/module-1/week-1/01_Variables_and_Datatypes/exercise/src/main/java/com/techelevator/Exercises.java
@@ -26,6 +26,9 @@ public class Exercises {
         3. 3 raccoons are playing in the woods. 2 go home to eat dinner. How
         many raccoons are left in the woods?
         */
+		int numberOfRaccoons = 3;
+		int numberOfRaccoonsThatLeft = 2;
+		int numberOfRaccoonsThatRemain = numberOfRaccoons - numberOfRaccoonsThatLeft;
 
 
         /* Exercise 4
