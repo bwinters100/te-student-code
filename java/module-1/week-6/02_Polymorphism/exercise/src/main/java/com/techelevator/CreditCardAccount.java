56610c2fbb6796065126c8be65d74fc7a72319de
Brent Winters <brentwinters100@gmail.com>
2023-08-04 12:07:53 -0400

    Poly some

---
+++ java/module-1/week-6/02_Polymorphism/exercise/src/main/java/com/techelevator/CreditCardAccount.java
@@ -0,0 +1,45 @@
+package com.techelevator;
+
+public class CreditCardAccount {
+    private String accountHolder;
+    private String accountNumber;
+    private int debt;
+
+    public CreditCardAccount(String accountHolder, String accountNumber){
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.debt = 0;
+    }
+    public int pay(int amountToPay){
+        this.debt = this.debt - amountToPay;
+        return this.debt;
+    }
+    public int charge(int amountToCharge){
+        this.debt = this.debt + amountToCharge;
+        return this.debt;
+    }
+    public int getBalance(){
+        return -this.debt;
+    }
+    public String getAccountHolder(){
+        return accountHolder;
+    }
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    public int getDebt(){
+        return debt;
+    }
+
+    public void setDebt(int debt) {
+        this.debt = debt;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+}
