d123219c09fa2e4669421f6d913c03696bea0dcf
Brent Winters <brentwinters100@gmail.com>
2023-09-19 00:04:52 -0400

    complete joins

--- java/module-2/week-2/01_Joins/exercise/Exercises/19_genre_count.sql
+++ java/module-2/week-2/01_Joins/exercise/Exercises/19_genre_count.sql
@@ -1,4 +1,7 @@
 -- 19. The genre name and the number of movies in each genre. Name the count column 'num_of_movies'.
 -- Order the results from the highest movie count to the lowest.
 -- (19 rows, the highest expected count is around 400).
-
+select genre_name, count(title) as num_of_movies from genre
+join movie_genre on movie_genre.genre_id = genre.genre_id
+join movie on movie.movie_id = movie_genre.movie_id
+group by genre_name order by num_of_movies desc
